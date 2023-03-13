1a78ab6fa8e7c4ce5770ab256f933e70b0b235e6
AnaM <test@gmail.com>
2023-03-12 21:53:56 -0500

    v2

--- factorial.js
+++ factorial.js
@@ -19,3 +19,69 @@ function factorial()
 
     return factorial;
 }
+
+// Capturar datos en variables en ambito local
+
+
+function numeros()
+{
+
+    let nombre;
+nombre = prompt();
+document.write(nombre)
+
+console.log(nombre);
+
+    let primero = frutas[0]
+
+}
+
+function cronometro ()
+{
+    let hora=0, minuto=0, segundo=0
+
+    while (true)
+    {
+        if (segundo<60)
+        {
+
+            segundo++;
+        }else{
+            segundo=0;
+            minuto++;
+
+            if(minuto<60)
+            {
+                minuto++;
+            }else{
+                minuto=0;
+                hora++;
+            }
+        }
+
+        //Pausa v1
+        
+        // let fecha = new Date();
+        // let fecha2 = new Date();
+
+        // do
+        // {
+        //     fecha2= new Date();
+        // } while (fecha2-fecha<1000)
+
+
+        //pausa V2
+        //setTimeout(call,1000);
+
+        // retraso 1seg
+        //Promise: objeto que representa la ejecucion de una funcion asincrona
+        new Promise (r=> setTimeout(r,1000));
+
+        console.log(hora+":"+minuto+":"+segundo);
+    }
+}
+
+// function call()
+// {
+//     console.log("termine.");
+// }
